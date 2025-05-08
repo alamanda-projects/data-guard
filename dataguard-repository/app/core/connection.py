e902076f15c6616120eb5e68b8f875c7151faa95
haninp <haninurchairilp@gmail.com>
2025-05-08 22:46:38 +0700

    menambah environemnt setup di docker-compose.yaml untuk data contract repo app

--- dataguard-repository/app/core/connection.py
+++ dataguard-repository/app/core/connection.py
@@ -8,12 +8,16 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from decouple import config
 
-string_connection = config("MONGODB_HOST")
-db = config("MONGODB_DB")
-col_dgr = config("MONGODB_COLLECTION_DGR")
-col_usr = config("MONGODB_COLLECTION_USR")
+host = config("MONGODB_HOST", default="dgr-db")
+user = config("MONGODB_USER", default="root")
+password = config("MONGODB_PASS", default="example")
+port = config("MONGODB_PORT", default="27017")
+db = config("MONGODB_DB", default="dgrdb")
+col_dgr = config("MONGODB_COLLECTION_DGR", default="dgr")
+col_usr = config("MONGODB_COLLECTION_USR", default="dgrusr")
 # db_usr = config("MONGODB_DB_USR")
 
+string_connection = f"mongodb://{user}:{password}@{host}:{port}/"
 client = AsyncIOMotorClient(string_connection)
 database = client[db]
 # database_usr = client[db_usr]
